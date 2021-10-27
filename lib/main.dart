9482ef7f4838a677d32e38b61ce077338266f0a5
joseph <24618839+josephgale@users.noreply.github.com>
2021-10-26 19:13:34 -0500

    replace build function

--- lib/main.dart
+++ lib/main.dart
@@ -7,26 +7,27 @@ void main() {
 class RecipeApp extends StatelessWidget {
   const RecipeApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+// 1
   @override
   Widget build(BuildContext context) {
+    // 2
+    final ThemeData theme = ThemeData();
+    // 3
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application....
-        // Testing comments code
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      // 4
+      title: 'Recipe Calculator',
+      // 5
+      theme: theme.copyWith(
+        colorScheme: theme.colorScheme.copyWith(
+          primary: Colors.grey,
+          secondary: Colors.black,
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // 6
+      home: const MyHomePage(title: 'Recipe Calculator'),
     );
   }
+
 }
 
 class MyHomePage extends StatefulWidget {
